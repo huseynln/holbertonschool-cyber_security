7707e69bf7fcc588456dc7d0bef539a58fe823d7
huseynln <huseynhastaken@gmail.com>
2026-05-20 11:47:54 +0000

    f

--- scripting_cyber/0x00-ruby_scripting/8-main.rb
+++ scripting_cyber/0x00-ruby_scripting/8-main.rb
@@ -1,5 +1,7 @@
+#!/usr/bin/env ruby
+
 require_relative '8-post'
 
 url = 'https://jsonplaceholder.typicode.com/posts'
-body_params = { title: 'Try to Post', body: "Okay that's good", userId: 11, id: 101 }
+body_params = {}
 post_request(url, body_params)
